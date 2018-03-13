694328dd875775a001d5fe2a2e5deb912bdba9bb
Yeiko <nativ.yeiko@gmail.com>
2018-03-13 19:38:35 +0100

    Victoire!

--- ressources/js/main.js
+++ ressources/js/main.js
@@ -1,119 +1,176 @@
-/* NAVIGATION */
+/* show and hide menu */
 
 $(document).ready(function(){
-  'use strict';
-  $(window).scroll(function(){
+
     'use strict';
-    if($(window).scrollTop() < 80 ) {
-      $('.navbar').css({
-        'margin-top': '-100px',
-        'opacity': '0'
-      });
-      $('.navbar-default').css({
-        'background-color': 'rgba(59, 59, 59, 0)'
-      })
-    } else {
-      $('.navbar').css ({
-        'margin-top': '0px',
-        'opacity': '1'
-      });
-      $('.navbar-default').css({
-        'background-color': 'rgba(59, 59, 59, 0.7)',
-        'border-color': '#444'
-      });
-      $('.navbar-brand img').css({
-        'height': '35px',
-        'padding-top': '0px;'
-      });
-      $('.navbar-nav > li > a').css({
-        'padding-top': '15px'
-      });
-    }
-  });
-});
 
-/*SMOOTH SCROLL*/
+    $(window).scroll(function() {
 
-$(document).ready(function() {
-		$('.nav-item, #scroll-to-top').on('click', function() { // Au clic sur un élément
-			var page = $(this).attr('href'); // Page cible
-			var speed = 750; // Durée de l'animation (en ms)
-			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
-			return false;
-		});
-	});
+      'use strict';
 
-/* MENU ACTIF AU CLIQUE */
+      if($(window).scrollTop() < 80 ) {
+        $('.navbar').css ({
+          'margin-top': '-100px',
+          'opacity': '0'
+        });
+        $('.navbar-default').css({
+          'background-color': 'rgba(59, 59, 59, 0)'
 
-$(document).ready(function(){
-  'use strict';
-  $('.navbar-nav li a').click(function(){
-    'use strict';
-    $('.navbar-nav li a').parent().removeClass("active");
-    $(this).parent().addClass("active");
-  });
-});
-
-/* MENU SURLIGNE AU SCROLL */
-
-$(document).ready(function(){
-  'use strict'
-  $(window).scroll(function(){
-    'use strict'
-    $("section").each(function(){
-      'use strict'
-      var bb = $(this).attr("id");
-      var hei = $(this).outerHeight();
-      var grttop = $(this).offset().top - 70;
-      if ($(window).scrollTop() > grttop && $(window).scrollTop() < grttop + hei) {
-        $(".navbar-nav li a[href='#" + bb + "']").parent().addClass("active");
+        });
       } else {
-        $(".navbar-nav li a[href='#" + bb + "']").parent().removeClass("active");
+        $('.navbar').css ({
+          'margin-top': '0px',
+          'opacity': '1'
+        });
+
+        $('.navbar-default').css({
+          'background-color': 'rgba(59, 59, 59, 0.7)',
+          'border-color': '#444'
+
+        });
+
+        $('.navbar-brand img').css({
+          'height': '35px',
+          'padding': '0px'
+
+        });
+
+        $('.navbar-nav > li > a').css({
+          'padding-top': '15px'
+        });
+
+
       }
     });
+
+
+
+});
+// add smooth scroling 
+
+$(document).ready(function(){
+
+    'use strict';
+
+        $('.nav-item, #scroll-to-top').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+               if (target.length) {
+                 $('html,body').animate({
+                     scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+});
+
+// active menu item on click
+$(document).ready(function(){
+
+    'use strict';
+
+    $('.navbar-nav li a').click(function(){
+
+        'use strict';
+
+        $('.navbar-nav li a').parent().removeClass("active");
+
+        $(this).parent().addClass("active");
+
+    });
+
+});
+
+//highlight menu item on scroll
+
+$(document).ready(function(){
+
+    'use strict';
+
+    $(window).scroll(function(){
+
+        'use strict';
+
+        $("section").each(function(){
+
+            'use strict';
+
+            var bb = $(this).attr("id");
+            var hei = $(this).outerHeight();
+            var grttop = $(this).offset().top - 70;
+
+            if($(window).scrollTop() > grttop && $(window).scrollTop() < grttop + hei){
+
+                $(".navbar-nav li a[href = '#" + bb + "']").parent().addClass("active");
+
+            } else{
+
+                $(".navbar-nav li a[href = '#" + bb + "']").parent().removeClass("active");
+            }
+
+        });
+
+    });
+});
+
+//add auto padding to header
+
+$(document).ready(function(){
+
+    'use strict';
+
+    setInterval(function(){
+
+        'use strict';
+
+        var windowHeight = $(window).height();
+        var containerHeight = $(".header-container").height();
+        var padTop = windowHeight - containerHeight;
+
+        $(".header-container").css({
+
+            'padding-top': Math.round(padTop / 2) + 'px',
+            'padding-bottom': Math.round(padTop / 2) + 'px'
+
+        });
+    }, 10)
+
+});
+
+
+//add bx slider
+
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+
+      slideWidth: 292.5,
+      auto: true,
+      minSlides: 1,
+      maxSlides: 3,
+      slideMargin: 50
   });
 });
 
-/* AUTO PADDING HEADER */
+// add counter
 
 $(document).ready(function(){
-  'use strict'
-  setInterval(function(){
-    'use strict'
-    var windowHeight = $(window).height();
-    var containerHeight = $(".header-container").height();
-    var padTop = windowHeight - containerHeight;
-    $(".header-container").css({
-      'padding-top': Math.round( padTop / 2) + 'px',
-      'padding-bottom': Math.round( padTop / 2) + 'px'
+
+    $('.counter-num').counterUp({
+
+        delay: 10,
+        time: 2000
     });
-  }, 10);
 });
 
-/* SLIDER */
+//add animation
 
 $(document).ready(function(){
-      $('.bxslider').bxSlider({
-        slideWidth: 292.5,
-        auto: true,
-        minSlides: 1,
-        maxSlides: 3,
-        sliderMargin: 50
-      });
-    });
 
-/* COUNTER */
+    'use strict';
+    new WOW().init();
 
-$(document).ready(function(){
-  $('.counter-num').counterUp({
-    delay: 10,
-    time: 2000
-  });
-});
-
-/* ANIMATIONS */
-
-$(document).ready(function(){
-  'use strict'
-  new WOW().init();
 });
