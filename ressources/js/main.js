f474b9f0c6c76469296bff8fbde61d1c8f2afbd0
Yeiko <nativ.yeiko@gmail.com>
2018-03-13 13:26:54 +0100

    js

--- ressources/js/main.js
+++ ressources/js/main.js
@@ -1,18 +1,31 @@
 /* Show and hide mene */
-
-$(document).ready(function(){
+$(document).ready(function () {
   'use strict';
-  $(window).scroll(function(){
+  $(window).scroll(function () {
     'use strict';
-    if($(window).scrolltop() <80){
+    if ($(window).scrollTop() < 80) {
       $('.navbar').css({
         'margin-top': '-100px',
         'opacity': '0'
       });
+      $('navbar-default').css({
+        'background-color': 'rgba(59,59,59,0)',
+      });
     } else {
       $('.navbar').css({
-        'margin-top': '-100px',
+        'margin-top': '0px',
         'opacity': '1'
+      });
+      $('navbar-default').css({
+        'background-color': 'rgba(59,59,59,0.7)',
+        'border-color': '#444'
+      });
+      $('.navbar-brand img').css({
+        'height': '35px',
+        'padding-top': '0px'
+      });
+      $('navbar-nav>li>a').css({
+        'padding-top': '15px'
       });
     }
   });
